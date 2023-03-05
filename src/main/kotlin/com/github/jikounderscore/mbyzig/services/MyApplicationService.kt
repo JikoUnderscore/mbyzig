83e1270c377fef32d71ffc4add1ac6c859aa2842
GitHub Action <action@github.com>
2023-03-05 21:34:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/jikounderscore/mbyzig/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jikounderscore/mbyzig/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jikounderscore.mbyzig.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jikounderscore.mbyzig.MyBundle
 
 class MyApplicationService {
 
